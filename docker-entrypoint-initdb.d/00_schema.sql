1012b08bf96e163e63582fa24638f4eb9d3a6322
ViktorT <tivi1990@mail.ru>
2021-10-03 15:41:45 +0200

    fixed #1

--- docker-entrypoint-initdb.d/00_schema.sql
+++ docker-entrypoint-initdb.d/00_schema.sql
@@ -18,12 +18,12 @@ CREATE TABLE cards
     id        BIGSERIAL PRIMARY KEY,
     "ownerId" BIGINT  NOT NULL REFERENCES users,
     number    TEXT    NOT NULL,
-    balance   BIGINT  NOT NULL DEFAULT 0 CONSTRAINT positive_balance CHECK (balance > 0 OR balance = 0),
+    balance   BIGINT  NOT NULL DEFAULT 0,
     active    BOOLEAN NOT NULL DEFAULT TRUE
 );
 CREATE TABLE roles
 (
-    id BIGINT PRIMARY KEY NOT NULL references users,
+    "userId" BIGINT PRIMARY KEY NOT NULL references users,
     role TEXT NOT NULL
 );
 
